43bc0554cb95ce2afd53e3d7b9b4aa08441d4fd5
Rahul Shriwas <rahulshriwas129378@gmail.com>
2026-01-21 01:35:28 +0530

    Replace react-tilt with react-parallax-tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,26 +8,25 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    className="xs:w-[250px] w-full"
+    tiltMaxAngleX={25}
+    tiltMaxAngleY={25}
+    scale={1.05}
+    transitionSpeed={450}
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -45,15 +44,16 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I ᴀᴍ ᴀ ғʀᴏɴᴛ-ᴇɴᴅ sᴏғᴛᴡᴀʀᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ sᴘᴇᴄɪᴀʟɪᴢɪɴɢ ɪɴ
-ʜɪɢʜ-ᴘᴇʀғᴏʀᴍᴀɴᴄᴇ, sᴄᴀʟᴀʙʟᴇ ᴡᴇʙ ᴀᴘᴘʟɪᴄᴀᴛɪᴏɴs ᴜsɪɴɢ Rᴇᴀᴄᴛ ᴀɴᴅ Nᴇxᴛ.ᴊs.
-I ᴄʀᴇᴀᴛᴇ ʀᴇғɪɴᴇᴅ, ʀᴇsᴘᴏɴsɪᴠᴇ ɪɴᴛᴇʀғᴀᴄᴇs ᴇɴɢɪɴᴇᴇʀᴇᴅ ғᴏʀ sᴘᴇᴇᴅ, ᴘʀᴇᴄɪsɪᴏɴ, ᴀɴᴅ sᴇᴀᴍʟᴇss ᴜsᴇʀ ᴇxᴘᴇʀɪᴇɴᴄᴇs. Wɪᴛʜ ᴀ sᴏʟɪᴅ ʙᴀᴄᴋᴇɴᴅ ғᴏᴜɴᴅᴀᴛɪᴏɴ, I ᴅᴇʟɪᴠᴇʀ sᴏʟᴜᴛɪᴏɴs ᴛʜᴀᴛ ᴀʀᴇ ʙᴏᴛʜ ᴠɪsᴜᴀʟʟʏ ᴄᴏᴍᴘᴇʟʟɪɴɢ ᴀɴᴅ ᴛᴇᴄʜɴɪᴄᴀʟʟʏ ʀᴏʙᴜsᴛ.
-
+        I ᴀᴍ ᴀ ғʀᴏɴᴛ-ᴇɴᴅ sᴏғᴛᴡᴀʀᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ sᴘᴇᴄɪᴀʟɪᴢɪɴɢ ɪɴ ʜɪɢʜ-ᴘᴇʀғᴏʀᴍᴀɴᴄᴇ,
+        sᴄᴀʟᴀʙʟᴇ ᴡᴇʙ ᴀᴘᴘʟɪᴄᴀᴛɪᴏɴs ᴜsɪɴɢ Rᴇᴀᴄᴛ ᴀɴᴅ Nᴇxᴛ.ᴊs. I ᴄʀᴇᴀᴛᴇ ʀᴇғɪɴᴇᴅ,
+        ʀᴇsᴘᴏɴsɪᴠᴇ ɪɴᴛᴇʀғᴀᴄᴇs ᴇɴɢɪɴᴇᴇʀᴇᴅ ғᴏʀ sᴘᴇᴇᴅ, ᴘʀᴇᴄɪsɪᴏɴ, ᴀɴᴅ sᴇᴀᴍʟᴇss ᴜsᴇʀ
+        ᴇxᴘᴇʀɪᴇɴᴄᴇs. Wɪᴛʜ ᴀ sᴏʟɪᴅ ʙᴀᴄᴋᴇɴᴅ ғᴏᴜɴᴅᴀᴛɪᴏɴ, I ᴅᴇʟɪᴠᴇʀ sᴏʟᴜᴛɪᴏɴs ᴛʜᴀᴛ
+        ᴀʀᴇ ʙᴏᴛʜ ᴠɪsᴜᴀʟʟʏ ᴄᴏᴍᴘᴇʟʟɪɴɢ ᴀɴᴅ ᴛᴇᴄʜɴɪᴄᴀʟʟʏ ʀᴏʙᴜsᴛ.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
